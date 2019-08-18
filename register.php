0f5ab7db22e7eb12b4dc0dac7f15f00f07b6f1b0
Alessandro Walter <alessandro.walter@outlook.de>
2019-08-18 15:56:03 +1000

    Implement redirection from register to login.

--- register.php
+++ register.php
@@ -1,3 +1,29 @@
+<?php
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    $user_entry = $_POST['username'] . '|' . $_POST['password'] . "\r\n";
+
+    $entries = explode("\n",
+        file_get_contents('gs://a1-2-users/users.txt')
+    );
+
+    array_push($entries, $user_entry);
+    $isWritten = file_put_contents('gs://a1-2-users/users.txt',
+        $entries);
+
+    if($isWritten === true) {
+        header('Location: login.php');
+    }
+    else {
+        #header("Location: https://cloudassignment1-2.appspot.com/login.php");
+        #echo "<script>window.location = 'http://www.google.com'</script>";
+        #echo "<script type='text/javascript'> document.location = 'login.php'; </script>";
+        ob_start();
+        header('Location: login.php');
+        exit();
+    }
+}
+?>
+
 <form method = "post" action="register.php">
     <h2>Register</h2>
     <div class="container">
@@ -14,25 +40,3 @@
         <button type="submit">Register</button>
     </div>
 </form>
-
-<?php
-if(isset($_POST['username']) && isset($_POST['password'])) {
-    $user_entry = $_POST['username'] . '|' . $_POST['password'] . "\r\n";
-
-    $entries = explode("\n",
-        file_get_contents('gs://a1-2-users/users.txt')
-    );
-
-    array_push($entries, $user_entry);
-    $isWritten = file_put_contents('gs://a1-2-users/users.txt',
-        $entries);
-
-    if($isWritten === false) {
-       die('Writing credentials to file failed');
-    }
-    else {
-       echo "Writing credentials to file succeeded";
-    }
-    echo $_POST['username'] . $_POST['password'];
-}
-?>
